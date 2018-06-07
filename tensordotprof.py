c96474445cac1c26536dfad9237c8750598ddd4f
Martin Ganahl <mganahl@mail.pi.local>
2018-06-07 12:11:27 -0400

    added a profiling function

---
+++ tensordotprof.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+#cmps program for ground state calculations of the inhomogeneous Lieb-Liniger model in the thermodynamic limit
+#in the following comments, [,] is the commutator and {,} is the anti commutator of operators
+#the following code employes the left gauge fixing through out, which results in l=11. Hence, l
+#does not appear in the code
+
+import sys,cProfile
+import numpy as np
+import sparsenumpy as snp
+import SparseTensor as spt
+import qutilities as utils
+import argparse
+
+import random
+import unittest
+
+comm=lambda x,y:np.dot(x,y)-np.dot(y,x)
+anticomm=lambda x,y:np.dot(x,y)+np.dot(y,x)
+herm=lambda x:np.conj(np.transpose(x))
+
+
+def TensDotProf(a,b,inds,reps):
+    for n in range(reps):
+        r=snp.tensordot(a,b,(inds,inds),ignore_qflow=True)
+
+
+
+
+
+parser = argparse.ArgumentParser('tensordotprof.py')
+parser.add_argument('--N', help='number of different quantum number sectors per leg (10)' ,type=int,default=10)
+parser.add_argument('--rank', help='tensor ranks' ,type=int,default=3)
+parser.add_argument('--dim', help='dimension of each quantum number sector on the legs (100)' ,type=int,default=100)
+args=parser.parse_args()
+        
+rank=args.rank
+N=args.N
+dim=args.dim
+kd={}
+
+for k,d in zip(range(N),[dim]*N):
+    kd[k]=d
+
+
+I=[]
+
+for n in range(rank):
+    I.append(spt.TensorIndex(kd,1))
+
+a=spt.SparseTensor.random(I)
+b=spt.SparseTensor.random(I)
+inds=range(rank)
+reps=1
+cProfile.run('TensDotProf(a,b,inds,reps)','tensdotprof')
